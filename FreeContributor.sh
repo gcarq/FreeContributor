0ab7d3b818eb1d4b980cec387f823bdea8585247
gcarq <michael.egger@tsn.at>
2016-06-26 11:52:59 +0200

    major code refactoring and 0.6.0 pump
    * enhanced domain sanitizing algorithm (+simple FQDN validator)
    * curl downloads and format convertion are now multi-threaded
    * outsourced domain list URIs to domainlist.conf
    * adapted argument handling and validating
    * added global config variables (DOMAINLIST_CONF, DEPENDENCIES, SUPPORTED_FORMATS, THREADS)
    * some performance improvements and code cleanups
    * fixed some minor bugs and adapted error handling for edge cases
    * slightly changed logging output

--- FreeContributor.sh
+++ FreeContributor.sh
@@ -2,7 +2,9 @@
 #
 # FreeContributor: Enjoy a safe and faster web experience
 # (c) 2016 by TBDS
+# (c) 2016 by gcarq
 # https://github.com/tbds/FreeContributor
+# https://github.com/gcarq/FreeContributor (forked)
 #
 # FreeContributor is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,381 +16,206 @@
 #
 # Dependencies:
 #  * GNU bash
-#  * GNU coreutils (sed, grep)
-#  * GNU wget or cURL
+#  * GNU awk
+#  * GNU coreutils (sed, grep, touch)
+#  * cURL
 #
-## Global Variables------------------------------------------------------------
-FREECONTRIBUTOR_VERSION='0.5.0'
-REDIRECTIP4="${REDIRECTIP4:=0.0.0.0}"
-REDIRECTIP6="${REDIRECTIP6:=::}"
+set -e
+source ./misc/banners.sh
 
+## Global Variables-------------------------------------------------------------------
+FREECONTRIBUTOR_VERSION='0.6.0'
+SUPPORTED_FORMATS=("none" "hosts" "dnsmasq" "unbound" "pdnsd")
+DEPENDENCIES=("sed" "grep" "curl")
+DOMAINLIST_CONF="domainlist.conf"
+
+# Set default values
+REDIRECTIP4="${REDIRECTIP4:=127.0.1.1}"
+REDIRECTIP6="${REDIRECTIP6:=::1}"
 TARGET="${TARGET:=$REDIRECTIP4}"
-FORMAT="${FORMAT:=dnsmasq}"
+FORMAT="${FORMAT:=none}"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Maximum threads (used for fetching and converting)
+THREADS=4
 
-# make temp files
-TMP=$(mktemp /tmp/data.XXXXX)
-DOMAINS=$(mktemp /tmp/domains.XXXXX)
+# Make temp files
+TMP_DOMAINS_RAW=$(mktemp /tmp/freecontribute-raw-domains.XXXXX)
+TMP_DOMAINS=$(mktemp /tmp/freecontribute-domains.XXXXX)
 
-## ----------------------------------------------------------------------------
+# Contains all domainlist URIs loaded from DOMAINLIST_CONF
+DOMAINLIST_URIS=()
+##------------------------------------------------------------------------------------
 
-welcome()
-{
-cat << EOF
-
-     _____               ____            _        _ _           _
-    |  ___| __ ___  ___ / ___|___  _ __ | |_ _ __(_) |__  _   _| |_ ___  _ __
-    | |_ | '__/ _ \/ _ \ |   / _ \| '_ \| __| '__| | '_ \| | | | __/ _ \| '__|
-    |  _|| | |  __/  __/ |__| (_) | | | | |_| |  | | |_) | |_| | || (_) | |
-    |_|  |_|  \___|\___|\____\___/|_| |_|\__|_|  |_|_.__/ \__,_|\__\___/|_|
-
-
-    Enjoy a safe and faster web experience
-
-    FreeContributor - http://github.com/tbds/FreeContributor
-    Released under the GPLv3 license
-    (c) 2016 tbds and contributors
-
-EOF
+panic() {
+  source ./misc/banners.sh
+  show_usage
+  printf "    ERROR: %s\n\n" "${1}" 1>&2
+  exit 1
 }
+export -f panic  # export function to sub shells
 
-usage()
-{
-cat << EOF
+check_dependencies() {
+  printf "    Checking dependencies...\n"
 
-
-    FreeContributor is a script to extract and convert domains lists from various sources.
-
-    USAGE:
-
-      $ $0 [-f format]  [-o out] [-t target]
-
-       -f format: specify an output format:
-
-          none        Extract domains only
-          hosts       Use hosts format
-          dnsmasq     dnsmasq as DNS resolver
-          unbound     unbound as DNS resolver
-          pdnsd       pdnsd as DNS resolver
-
-       -o out: specify an output file
-
-       -t target: specify the target
-
-          default:     $TARGET
-                       $REDIRECTIP6
-                       NXDOMAIN
-                       custom (e.g. 192.168.1.20)
-
-       -help: show this help
-
-
-    EXAMPLES:
-
-      $ $0 -f hosts -t 0.0.0.0 -o blacklist.conf
-
-      $ $0 -f dnsmasq -t NXDOMAIN -o blacklist.conf
-
-EOF
-}
-
-
-check_dependencies()
-{
-  programs=( sed grep curl )
-
-  for prg in "${programs[@]}"
-  do
-    echo -e "\n\t Checking if $prg is installed ..."
-    type -P $prg &>/dev/null || (echo -e "\tmissing: $prg"; exit 1)
+  for prg in "${DEPENDENCIES[@]}"; do
+    type -P $prg &>/dev/null || \
+      panic "Unable to find executable \"${prg}\" in PATH. (Make sure it is installed properly)"
   done
 }
 
+load_domainlist_conf() {
+  # Check if domainlist conf is readable
+  if [ ! -r "${DOMAINLIST_CONF}" ]; then
+    panic "Unable to load domainlist config: \"${DOMAINLIST_CONF}\""
+  fi
 
-##
-## See FilterLists for a comprehensive list of filter lists from all over the web
-## https://filterlists.com/
-##
+  # Load sanitze and load URIs
+  DOMAINLIST_URIS=($(sed -r "s/#.*$//;           \
+                             s/^[[:space:]]*//;  \
+                             /^[[:space:]]*$/d;  \
+                             s/[[:space:]]*$//;  \
+                            " ${DOMAINLIST_CONF} | awk '!x[$0]++'))
 
-download_sources()
-{
+  printf "    Imported %d domain list URI(s).\n" ${#DOMAINLIST_URIS[@]}
+}
 
-sources=(
-##
-## Use StevenBlack/hosts mirrors to save bandwidth from original servers
-## https://github.com/StevenBlack/hosts/tree/master/data
-##  'https://adaway.org/hosts.txt'
-    'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/adaway.org/hosts'
-##  'http://www.malwaredomainlist.com/hostslist/hosts.txt'
-    'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/malwaredomainlist.com/hosts'
-##  'http://winhelp2002.mvps.org/hosts.txt'
-    'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/mvps.org/hosts'
-##  'http://someonewhocares.org/hosts/hosts'
-    'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/someonewhocares.org/hosts'
-##  'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext'
-    'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/yoyo.org/hosts'
-    'https://raw.githubusercontent.com/StevenBlack/hosts/master/extensions/gambling/hosts'
-    'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/tyzbit/hosts'
-    'https://raw.githubusercontent.com/gorhill/uMatrix/master/assets/umatrix/blacklist.txt'
-    'https://raw.githubusercontent.com/quidsup/notrack/master/trackers.txt'
-    'https://raw.githubusercontent.com/Dawsey21/Lists/master/main-blacklist.txt'
-    'http://sysctl.org/cameleon/hosts'
-    'http://malwaredomains.lehigh.edu/files/justdomains'
-    'https://isc.sans.edu/feeds/suspiciousdomains_High.txt'
-    'https://raw.githubusercontent.com/zant95/hosts/master/hosts'
-    'https://www.dshield.org/feeds/suspiciousdomains_Low.txt'
-    'https://feodotracker.abuse.ch/blocklist/?download=domainblocklist'
-    'https://palevotracker.abuse.ch/blocklists.php?download=domainblocklist'
-    'https://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt'
-    'https://ransomwaretracker.abuse.ch/downloads/CW_C2_DOMBL.txt'
-    'https://ransomwaretracker.abuse.ch/downloads/CW_PS_DOMBL.txt'
-    'https://ransomwaretracker.abuse.ch/downloads/TC_C2_DOMBL.txt'
-    'https://ransomwaretracker.abuse.ch/downloads/TC_PS_DOMBL.txt'
-    'https://ransomwaretracker.abuse.ch/downloads/LY_C2_DOMBL.txt'
-    'https://ransomwaretracker.abuse.ch/downloads/LY_PS_DOMBL.txt'
-    'https://ransomwaretracker.abuse.ch/downloads/TL_C2_DOMBL.txt'
-    'https://ransomwaretracker.abuse.ch/downloads/TL_PS_DOMBL.txt'
-    'https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist'
-    'http://adblock.gjtech.net/?format=unix-hosts'
-#    'http://hostsfile.mine.nu/Hosts'
-##
-## https://github.com/crazy-max/WindowsSpyBlocker
-##
-#    'https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/blob/master/hosts/windows10_extra.txt'
-#    'https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/blob/master/hosts/windows10_spy.txt'
-#    'https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/blob/master/hosts/windows10_update.txt'
-##
-## Terms of Use of hpHosts
-## This service is free to use, however, any and ALL automated use is
-## strictly forbidden without express permission from ourselves
-##    'http://hosts-file.net/ad_servers.txt'
-##    'http://hosts-file.net/hphosts-partial.txt'
+fetch_domainlists() {
+  printf "    Fetching lists (threads: %d)...\n" ${THREADS}
 
+  printf '%s\n' ${DOMAINLIST_URIS[@]} | xargs -n1 -I{} -P ${THREADS} \
+    bash -c "if ! curl -L '{}' 2>/dev/null >>'${TMP_DOMAINS_RAW}'; then panic 'Unable to fetch domain list: {}'; fi"
 
-## error 'http://securemecca.com/Downloads/hosts.txt'
-#    'http://www.joewein.net/dl/bl/dom-bl.txt'
-#    'http://adblock.mahakala.is/'
-#    'http://mirror1.malwaredomains.com/files/justdomains'
-#    'https://raw.githubusercontent.com/CaraesNaur/hosts/master/hosts.txt'
-#SSL cerificate 'https://elbinario.net/wp-content/uploads/2015/02/BloquearPubli.txt'
-#
-#    'https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
-#    'https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt'
-#    'https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt'
-#    'https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt'
-#
-# https://github.com/brucebot/pisoft
-#
-#    'http://code.taobao.org/svn/adblock/trunk/hosts.txt'
-#    'http://hostsfile.org/Downloads/BadHosts.unx.zip'
-#    'http://support.it-mate.co.uk/downloads/HOSTS.txt'
-#    'https://hosts.neocities.org/'
-#    'https://publicsuffix.org/list/effective_tld_names.dat'
-#    'http://cdn.files.trjlive.com/hosts/hosts-v8.txt'
-#    'http://tcpdiag.dl.sourceforge.net/project/adzhosts/HOSTS.txt'
-#    'http://optimate.dl.sourceforge.net/project/adzhosts/HOSTS.txt'
-#    'https://openphish.com/feed.txt'
-#    'https://easylist-downloads.adblockplus.org/rolist+easylist.txt'
-#    'http://www.shallalist.de/Downloads/shallalist.tar.gz'
-#    'http://support.it-mate.co.uk/downloads/HOSTS.txt'
-#    'https://raw.githubusercontent.com/reek/anti-adblock-killer/master/anti-adblock-killer-filters.txt'
-#    'http://www.sa-blacklist.stearns.org/sa-blacklist/sa-blacklist.current'
-#    'https://easylist-downloads.adblockplus.org/malwaredomains_full.txt'
-#    'https://easylist-downloads.adblockplus.org/easyprivacy.txt'
-#    'https://easylist-downloads.adblockplus.org/easylist.txt'
-#    'https://easylist-downloads.adblockplus.org/fanboy-annoyance.txt'
-#    'http://www.fanboy.co.nz/adblock/opera/urlfilter.ini'
-#    'http://www.fanboy.co.nz/adblock/fanboy-tracking.txt'
-)
+  printf "    Got $(wc -l < ${TMP_DOMAINS_RAW}) dns records.\n"
+}
 
-  echo -e "\n\t FreeContributor is downloading data ..."
-  for item in ${sources[*]}
-  do
-     echo -e "\n\t :: Downloading from URL: $item"
-     curl -s $item >> $TMP || { echo -e "\n\t Error downloading $item"; exit 1; }
+sanitize_merged_domainlist() {
+  REGEX_IPV4_VALIDATION="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.)\
+{3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+
+  ## Replacments are done in the following order:
+  #   > strip comments starting with '#'
+  #   > replace substr '127.0.0.1' with ''
+  #   > replace substr '0.0.0.0' with ''
+  #   > strip all ^M characters (windows line ending character)
+  #   > ltrim tabs and whitespaces
+  #   > rtrim tabs and whitespaces
+  #   > remove lines which only contain an ipv4 addresses
+  #   > remove 'localhost' lines
+  #   > delete empty lines
+
+  # Apply replacements
+  sed -ir "s/#.*$//;                     \
+           s/127.0.0.1//;                \
+           s/0.0.0.0//;                  \
+           s/\^M//g;                     \
+           s/^[[:space:]]*//;            \
+           s/[[:space:]]*$//;            \
+           s/${REGEX_IPV4_VALIDATION}//; \
+           /^localhost$/d;               \
+           /^[[:space:]]*$/d;            \
+          " ${TMP_DOMAINS_RAW}
+
+  # Remove duplicate lines
+  awk -i inplace '!x[$0]++' ${TMP_DOMAINS_RAW}
+
+  # Remove invalid FQDNs (https://en.wikipedia.org/wiki/Fully_qualified_domain_name)
+  grep -Po '(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)' \
+    ${TMP_DOMAINS_RAW} > ${TMP_DOMAINS}
+}
+
+gen_hosts_conf() {
+  if [ ${TARGET} = "NXDOMAIN" ]; then
+    # Not supported
+    panic "hosts format does not support target NXDOMAIN."
+  else
+    # 127.0.1.1       example.tld
+    ./misc/gen-hosts-header.sh > "${OUTPUTFILE}" # Generate hosts header
+    printf "%s\n" $(cat ${TMP_DOMAINS}) | xargs -n1 -I{} -P ${THREADS} \
+    printf "%s %s\n" "${TARGET}" "{}"  > "${OUTPUTFILE}"
+  fi
+}
+
+gen_dnsmasq_conf() {
+  if [ ${TARGET} = "NXDOMAIN" ]; then
+    # server=/example.tld/
+    printf "server=/%s/\n" $(cat ${TMP_DOMAINS})  > "${OUTPUTFILE}"
+  else
+    # address=/example.tld/127.0.1.1
+    printf "%s\n" $(cat ${TMP_DOMAINS}) | xargs -n1 -I{} -P ${THREADS} \
+      printf "address=/%s/%s\n" "{}" "${TARGET}" > "${OUTPUTFILE}"
+  fi
+}
+
+gen_unbound_conf() {
+  if [ ${TARGET} = "NXDOMAIN" ]; then
+    # local-zone: "example.tld" static
+    printf "local-zone: \"%s\" static\n" $(cat ${TMP_DOMAINS})  > "${OUTPUTFILE}"
+  else
+    # local-zone: "example.tld" redirect
+    # local-data: "example.tld A 127.0.1.1"
+    printf "%s\n" $(cat ${TMP_DOMAINS}) | xargs -n1 -I{} -P ${THREADS} \
+      printf "local-zone: \"%s\" redirect\nlocal-data: \"%s A %s\"\n" \
+        "{}" "{}" "${TARGET}" > "${OUTPUTFILE}"
+  fi
+}
+
+gen_pdnsd_conf() {
+  if [ ${TARGET} = "NXDOMAIN" ]; then
+    # neg { name=example.tld; types=domain; }
+    printf "neg { name=%s; types=domain; }\n" $(cat ${TMP_DOMAINS}) > "${OUTPUTFILE}"
+  else
+    # Not implemented
+    panic "NXDOMAIN is currently the only implementation for pdnsd."
+  fi
+}
+
+parse_arguments() {
+  local OPTIND OPTARG
+  # Match parameters
+  while getopts ":t:f:o:h" opt; do
+    case $opt in
+      h)  show_usage; exit 0;;
+      f)  FORMAT="${OPTARG}";;
+      o)  OUTPUTFILE="${OPTARG}";;
+      t)  TARGET="${OPTARG}";;
+      \?) panic "Invalid option: -${OPTARG}";;
+    esac
   done
-}
+  shift "$((OPTIND-1))"
 
-extract_domains()
-{
-# clean this code with better regex
-# https://blog.mister-muffin.de/2011/11/14/adblocking-with-a-hosts-file/
-# sed 's/\([^#]*\)#.*/\1/;s/[ \t]*$//;s/^[ \t]*//;s/[ \t]\+/ /g'
-#
-
-  echo -e "\n\t Extracting domains from previous lists ..."
-  # remove empty lines and comments
-  grep -Ev '^$' $TMP | \
-  grep -o '^[^#]*'  | \
-  # exclude locahost entries
-  grep -v "localhost" | \
-  # remove 127.0.0.1 and 0.0.0.0
-  sed 's/127.0.0.1//;s/0.0.0.0//' | \
-  # remove tab and spaces in the begining
-  sed -e 's/^[ \t]*//' | \
-  # remove ^M
-  sed 's/\r//g' | grep -Ev '^$' | \
-  sort | uniq > $DOMAINS
-
-  echo -e "\n\t Domains extracted using ${FORMAT} format: $(cat ${DOMAINS} | wc -l )"
-
-}
-
-## none -------------------------------------------------------------------
-
-domains()
-{
-  cp ${DOMAINS} > "${OUTPUTFILE}"
-}
-
-## hosts ------------------------------------------------------------------
-
-hosts()
-{
-  #Generate hosts header
-  ./utils/hosts.header.sh
-
-  mv hosts.header "${OUTPUTFILE}"
-
-  while read line; do
-    echo "${TARGET} ${line}" >> "${OUTPUTFILE}"
-  done < $DOMAINS
-
-}
-
-
-## dnsmasq -----------------------------------------------------------------
-
-dnsmasq()
-{
-  if [ ${TARGET} = "NXDOMAIN" ]; then
-    awk '{print "server=/"$1"/"}' $DOMAINS > "${OUTPUTFILE}"
-
+  # Validate output argument
+  if [ -z ${OUTPUTFILE+x} ]; then
+    panic "You must specify an output file."
   else
-    awk -v var="${TARGET}" '{print "address=/"$1"/"var}' $DOMAINS > "${OUTPUTFILE}"
+    # Check if we have write access
+    touch "${OUTPUTFILE}" > /dev/null 2>&1 || \
+      panic "Unable to create to output file: ${OUTPUTFILE}"
+  fi
+
+  # Validate format argument
+  if [[ ! " ${SUPPORTED_FORMATS[@]} " =~ " ${FORMAT} " ]]; then
+    panic "format must be one of {none|hosts|dnsmasq|unbound|pdnsd}."
   fi
 }
 
-## unbound -----------------------------------------------------------------
+## Main ------------------------------------------------------------------------
 
-unbound()
-{
-  if [ ${TARGET} = "NXDOMAIN" ]; then
-
-#local-zone: "testdomain.test" static
-#http://www.unixcl.com/2012/07/print-double-quotes-in-unix-awk.html
-
-    awk '{print "local-zone: ""\x22"$1"\x22"" static"}' $DOMAINS > "${OUTPUTFILE}"
-
-  else
-
-#https://github.com/jodrell/unbound-block-hosts/
-#https://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound
-#local-zone: "example.tld" redirect
-#local-data: "example.tld A 127.0.0.1"
-
-    while read line; do
-      echo "local-zone: \"${line}\" redirect" >> "${OUTPUTFILE}"
-      echo "local-data: \"${line} A ${TARGET}\"" >> "${OUTPUTFILE}"
-    done < $DOMAINS
-  fi
-}
-
-## pdnsd -------------------------------------------------------------------
-
-pdnsd()
-{
-#https://news.ycombinator.com/item?id=3035825
-#https://pgl.yoyo.org/adservers/serverlist.php?hostformat=pdnsd
-#neg { name=example.tld; types=domain; }
-
-  awk '{print "neg { name="$1"; types=domain; }"}' $DOMAINS > "${OUTPUTFILE}"
-}
-
-
-finish()
-{
-cat <<'EOF'
-
-    FreeContributor blocklist generated.
-    Enjoy surfing in the web
-
-    More info at https://tbds.github.io/FreeContributor/
-
-EOF
-}
-
-
-## Main --------------------------------------------------------------------
-
-welcome
-
-#Check the number of arguments. If none are passed, print help and exit.
-NUMARGS=$#
-
-if [ $NUMARGS -eq 0 ]; then
-  usage
-  exit 1
-fi
-
-
-while getopts ":t:f:o:h" opt; do
-  case $opt in
-    h)
-      usage
-      exit 1
-    ;;
-    f)
-      FORMAT="$OPTARG"
-    ;;
-    o)
-      OUTPUTFILE="$OPTARG"
-    ;;
-    t)
-      TARGET="$OPTARG"
-    ;;
-    \?)
-      echo -e "\n\tInvalid option: -$OPTARG" >&2
-      usage
-      exit 1
-    ;;
-  esac
-done
-
-shift "$((OPTIND-1))"
-
-if [ -z ${OUTPUTFILE+x} ]; then
-  echo -e "\n\tYou must specify an output file"
-  usage
-  exit 1
-fi
-
+show_header
+parse_arguments "$@"
 check_dependencies
-download_sources
-extract_domains
+load_domainlist_conf
+fetch_domainlists
+sanitize_merged_domainlist
 
-case "$FORMAT" in
-  "none")
-    domains
-  ;;
-  "hosts")
-    hosts
-  ;;
-  "dnsmasq")
-    dnsmasq
-  ;;
-  "unbound")
-    unbound
-  ;;
-  "pdnsd")
-    pdnsd
-  ;;
-   *)
-    echo -e "\n\tInvalid option"
-    usage
-    exit 1
-  ;;
+# Generate list for specified format
+case "${FORMAT}" in
+  "none")    cp "${TMP_DOMAINS}" "${OUTPUTFILE}";;
+  "hosts")   gen_hosts_conf;;
+  "dnsmasq") gen_dnsmasq_conf;;
+  "unbound") gen_unbound_conf;;
+  "pdnsd")   gen_pdnsd_conf;;
 esac
+printf "\n    Domain list generated (format: %s, records: %s, filename: %s).\n" \
+       "${FORMAT}" $(cat ${TMP_DOMAINS} | wc -l) "${OUTPUTFILE}"
 
-finish
+printf "    Enjoy surfing in the web.\n\n"
